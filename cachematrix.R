cccfe98b9814b536ad54ab25665a775aa85d6516
Balázs Papp <balazs.papp@norbit.com>
2017-05-09 23:27:38 +0200

    Implemented predefined function dummies
    	- makeCacheMatrix
    	- cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix is for creating a special matrix object that can store its cached
+## value. The matrix can be set, get and the inverse can be set and queried.
+## cacheSolve returns the cached inverse value if possible. If not previously
+## set, calculates and returns the inverse of the matrix.
 
-## Write a short comment describing this function
-
+## Creates a special matrix object that can store its cached value
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of 'x'.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
